795eceaa6b441ea59c8bf707998f919885498184
MacJ13 <jacdim02@gmail.com>
2023-02-22 15:24:41 +0100

    Modify regex object
    Also create inputs variable with all dom input selectors

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const confirmInput = document.getElementById('confirmpass');
 const firstNameInput = document.getElementById('firstname');
 const phoneInput = document.getElementById('phone');
 
+const inputs = document.querySelectorAll('input');
+
 const regex = {
-    tel : "\d{3}[\-]\d{3}[\-]\d{4}/",
-    text: "^[a-zA-Z][a-z]{2,}$",
-    email : "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$",
-    password : "(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+    tel : new RegExp(/\d{3}[\-]\d{3}[\-]\d{4}/),
+    text: new RegExp(/^[a-zA-Z][a-z]{2,}$/),
+    email : new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+    password : new RegExp(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
 };
