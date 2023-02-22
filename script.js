74ef72dcdfb2cfa52a3923f7ba343d5ead230401
MacJ13 <jacdim02@gmail.com>
2023-02-22 15:33:42 +0100

    Set property 'noValidate' in form element on true value

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ const confirmInput = document.getElementById('confirmpass');
 const firstNameInput = document.getElementById('firstname');
 const phoneInput = document.getElementById('phone');
 
+// all input element in signup form
 const inputs = document.querySelectorAll('input');
 
+// object, with regex properties
 const regex = {
     tel : new RegExp(/\d{3}[\-]\d{3}[\-]\d{4}/),
     text: new RegExp(/^[a-zA-Z][a-z]{2,}$/),
     email : new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
     password : new RegExp(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
 };
+
+// set noValidate property to true, then data aren'te be validated in html
+formElement.noValidate = true;
+
+
+
