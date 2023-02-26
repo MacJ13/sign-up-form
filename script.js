13e971ca885648b6adb344ae47902d5deb504178
MacJ13 <jacdim02@gmail.com>
2023-02-26 20:38:06 +0100

    Remove unneeded input element variables

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 "use strict";
 
-const formElement = document.querySelector('.form');
-const emailInput = document.getElementById('email');
+// password input fields
 const passwordInput = document.getElementById('password');
 const confirmInput = document.getElementById('confirmpass');
-const firstNameInput = document.getElementById('firstname');
-const phoneInput = document.getElementById('phone');
 
 // all input element in signup form
 const inputs = document.querySelectorAll('input');
